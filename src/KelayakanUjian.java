44e0d88c3516b048e25874a52cb5fabde15ca2ae
Chaniel Siregar <2582024@unai.edu>
2026-09-21 09:56:43 +0700

    Tambah komentar kesimpulan precedence dan !dispensasi

--- src/KelayakanUjian.java
+++ src/KelayakanUjian.java
@@ -16,10 +16,25 @@ public class KelayakanUjian {
         boolean b = (kehadiran >= 75 && nilaiTugas >= 60) || dispensasi;
         boolean c = kehadiran >= 75 && (nilaiTugas >= 60 || dispensasi);
 
+        /*
+         * HASIL PERBANDINGAN:
+         * Versi yang sama dengan a adalah b. Versi c berbeda dari a.
+         *
+         * KESIMPULAN: operator && punya precedence lebih tinggi daripada ||,
+         * jadi Java membaca a sebagai (kehadiran >= 75 && nilaiTugas >= 60)
+         * || dispensasi, persis seperti b. Pada c, kurung menggeser urutan
+         * sehingga || dihitung lebih dulu dan hasilnya bisa berbeda, misalnya
+         * pada kehadiran 60, nilai tugas 80, dispensasi true:
+         * a = true, b = true, c = false.
+         */
+
+        boolean tidakDispensasi = !dispensasi;
+
         System.out.println();
         System.out.println("a (tanpa kurung)      : " + a);
         System.out.println("b (kurung precedence) : " + b);
         System.out.println("c (kurung digeser)    : " + c);
+        System.out.println("!dispensasi           : " + tidakDispensasi);
 
         input.close();
     }
