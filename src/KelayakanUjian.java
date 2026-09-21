fc31d4447d16e8a8eaba3b1ff920e6e3aef1e799
Chaniel Siregar <2582024@unai.edu>
2026-09-21 10:00:04 +0700

    Tambah bukti short-circuit dengan cek++ beserta penjelasan

--- src/KelayakanUjian.java
+++ src/KelayakanUjian.java
@@ -30,11 +30,27 @@ public class KelayakanUjian {
 
         boolean tidakDispensasi = !dispensasi;
 
+        // Bukti short-circuit
+        int cek = 0;
+        boolean x = (kehadiran >= 75) && (cek++ >= 0);
+        boolean y = (nilaiTugas >= 60) || (cek++ >= 0);
+
+        /*
+         * PENJELASAN cek:
+         * Pada input contoh (kehadiran 60, nilai tugas 80) nilai cek tetap 0.
+         * Pada x, sisi kiri (kehadiran >= 75) bernilai false, jadi && sudah
+         * pasti false dan sisi kanan (cek++ >= 0) tidak pernah dievaluasi.
+         * Pada y, sisi kiri (nilaiTugas >= 60) bernilai true, jadi || sudah
+         * pasti true dan sisi kanan juga dilewati. Karena cek++ tidak jalan
+         * sama sekali, cek berakhir di 0. Itulah short-circuit evaluation.
+         */
+
         System.out.println();
         System.out.println("a (tanpa kurung)      : " + a);
         System.out.println("b (kurung precedence) : " + b);
         System.out.println("c (kurung digeser)    : " + c);
         System.out.println("!dispensasi           : " + tidakDispensasi);
+        System.out.println("cek dipanggil         : " + cek);
 
         input.close();
     }
